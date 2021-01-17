93411b19c55fc15226a72a6b0fdc74523f44dc0a
aditisinha-accolite <aditi.sinha@accolitedigital.com>
2021-01-17 23:25:56 +0530

    adding angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngPro';
+  allUser: any;
+  isEdit = false;
+
+  userObj={
+    name:'',
+    Eid:'',
+    age:'',
+    dob:'',
+    id:''
+  }
+
+  constructor(private commonService:CommonService){}
+
+  ngOnInit(){
+    this.getLatestUser()
+  }
+  addUser(formObj: any){
+    // console.log(formObj)
+    this.commonService.createUser(formObj).subscribe((response)=>{
+      this.getLatestUser();   
+    })
+  }
+  getLatestUser(){
+    this.commonService.getAllUser().subscribe((response)=>{
+      this.allUser = response;
+      // console.log(this.allUser);  
+    })
+  }
+  editUser(user:any){
+    this.isEdit=true;
+    this.userObj=user;
+  }
+  deleteUser(user:any){
+    // console.log(user)
+     this.commonService.deleteUser(user).subscribe(()=>{
+       this.getLatestUser();
+     })
+  }
+  updateUser(){
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(()=>{
+      this.getLatestUser();
+    })
+  }
 }
